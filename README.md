d6ebe58f22f654377b975b3bf16ad40571cfa8b8
dungngminh <63831488+dungngminh@users.noreply.github.com>
2025-06-30 20:22:45 +0700

    add borrow and ref

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rusty Hotpot 🦀 🍲
+
+This is a place to show what I've learnt about Rust, all about this language will be here, like a hotpot =))
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,84 @@
-use std::io;
-
-
 fn main() {
-    println!("Hello, world!");
+    // mutable and immutable
+    println!("This is main");
 
-    println!("Guess a number:");
+    let x: i32 = 5;
 
-    let mut input = String::new();
+    println!("x value is {x}");
 
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read value");
+    let mut mut_x: i32 = 5;
 
-    println!("You guessed: {}", input);
+    println!("mut_x before changed is {mut_x}");
+
+    mut_x += 1;
+
+    println!("mut_x after changed is {mut_x}");
+
+    // Control flow
+
+    if x > 5 {
+        println!("x is larger than 5")
+    } else if x < 5 {
+        println!("No, x is smaller than 5")
+    } else {
+        println!("X equals 5")
+    }
+
+    // Normal loop
+
+    let mut counter: i32 = 0;
+
+    let result_after_loop = loop {
+        counter += 1;
+
+        if counter >= 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("result after normal loop is {}", result_after_loop);
+
+    // For loop
+
+    let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+    for a in arr {
+        println!("value {a}")
+    }
+
+    let mut s = String::from("I'm");
+
+    let s1 = s.clone();
+
+    s.push_str(", Dzung");
+
+    println!("{s}");
+
+    println!("{s1}");
+
+    test_fuction(&mut s);
+
+    println!("{s}");
+
+    let s2 = test_return_string(&mut s);
+
+    println!("{s2}");
+
+    let s3 = test_return_string(&mut s);
+
+    println!("{s3}");
+}
+
+fn test_fuction(s: &mut String) {
+    println!("s in test_function is {s}");
+
+    s.push_str(", Dzung");
+
+    println!("s in test_function is {s}");
+}
+
+fn test_return_string(s: &mut String) -> &String {
+    s.push_str(", DzungNgMinh");
+
+    s
 }
